d21a00ab1a5f4d09969edc0a1061076e2507df3e
blanclt <boyvip3042k@gmail.com>
2023-07-31 14:40:35 +0700

    grafbase

---
+++ components/AuthProvider.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+const AuthProvider = () => {
+  return <div>AuthProvider</div>;
+};
+
+export default AuthProvider;
